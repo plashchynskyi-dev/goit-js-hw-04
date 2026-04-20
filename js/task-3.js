0ad977285d31db8015b5d4d920a26a169450378e
Andrew <plaschinskiyandriy@gmail.com>
2026-04-20 12:26:27 +0300

    Update task-3.js

--- js/task-3.js
+++ js/task-3.js
@@ -1,10 +1,25 @@
-function getElementWidth(content , padding , border ) {
-  const contentWidth = Number.parseFloat(content);
-  const paddingWidth = Number.parseFloat(padding);
-  const borderWidth = Number.parseFloat(border);
-  return contentWidth + paddingWidth * 2 + borderWidth * 2;
-}
+const profile = {
+  username: "Jacob",
+  playTime: 300,
 
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
+  changeUsername(newName) {
+    this.username = newName;
+  },
+
+  updatePlayTime(hours) {
+    this.playTime += hours;
+  },
+
+  getInfo() {
+    return `${this.username} has ${this.playTime} active hours!`;
+  }
+};
+
+
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+
+profile.changeUsername("Marco");
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
+
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
