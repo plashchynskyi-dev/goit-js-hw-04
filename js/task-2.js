4bdc029f76c5df3edadcd50ac5e5435e45649aaa
Andrew <plaschinskiyandriy@gmail.com>
2026-04-20 12:34:46 +0300

    Update task-2.js

--- js/task-2.js
+++ js/task-2.js
@@ -1,6 +1,12 @@
 function calcAverageCalories(days) {
-  if (!days.length) return 0;
-  const totalCalories = days.reduce((sum, day) => sum + day.calories, 0);
+  if (days.length === 0) return 0;
+
+  let totalCalories = 0;
+
+  for (let i = 0; i < days.length; i++) {
+    totalCalories += days[i].calories;
+  }
+
   return totalCalories / days.length;
 }
 
