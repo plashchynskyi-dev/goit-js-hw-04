693a161f426b242a0816d605dce8d892768a16d3
Andrew <plaschinskiyandriy@gmail.com>
2026-05-15 17:39:03 +0300

    Update task-2.js

--- js/task-2.js
+++ js/task-2.js
@@ -1,38 +1,38 @@
 function calcAverageCalories(days) {
-  if (days.length === 0) return 0;
-
+  let totaldays = 0;
   let totalCalories = 0;
-
-  for (let i = 0; i < days.length; i++) {
-    totalCalories += days[i].calories;
+  for (const day of days) {
+    totalCalories += day.calories;
+    totaldays += 1;
   }
-
-  return totalCalories / days.length;
+  if (totaldays === 0) {
+    return 0;
+  }
+  return totalCalories / totaldays;
 }
 
+console.log(
   calcAverageCalories([
-    { day: "monday", calories: 3010 },
-    { day: "tuesday", calories: 3200 },
-    { day: "wednesday", calories: 3120 },
-    { day: "thursday", calories: 2900 },
-    { day: "friday", calories: 3450 },
-    { day: "saturday", calories: 3280 },
-    { day: "sunday", calories: 3300 }
+    { day: 'monday', calories: 3010 },
+    { day: 'tuesday', calories: 3200 },
+    { day: 'wednesday', calories: 3120 },
+    { day: 'thursday', calories: 2900 },
+    { day: 'friday', calories: 3450 },
+    { day: 'saturday', calories: 3280 },
+    { day: 'sunday', calories: 3300 },
   ])
 ); // 3180
 
 console.log(
   calcAverageCalories([
-    { day: "monday", calories: 2040 },
-    { day: "tuesday", calories: 2270 },
-    { day: "wednesday", calories: 2420 },
-    { day: "thursday", calories: 1900 },
-    { day: "friday", calories: 2370 },
-    { day: "saturday", calories: 2280 },
-    { day: "sunday", calories: 2610 }
+    { day: 'monday', calories: 2040 },
+    { day: 'tuesday', calories: 2270 },
+    { day: 'wednesday', calories: 2420 },
+    { day: 'thursday', calories: 1900 },
+    { day: 'friday', calories: 2370 },
+    { day: 'saturday', calories: 2280 },
+    { day: 'sunday', calories: 2610 },
   ])
 ); // 2270
 
-console.log(
-  calcAverageCalories([])
-); // 0
+console.log(calcAverageCalories([])); // 0
